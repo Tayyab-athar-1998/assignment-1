4b3e2b5133bbf2392f60c380246e8d399b271d7b
Tayyab Athar <tayyabathar@Tayyabs-MacBook-Pro.local>
2020-12-09 21:56:48 +0500

    adding for person with join

--- app/models/person_with_join.rb
+++ app/models/person_with_join.rb
@@ -5,4 +5,21 @@ class PersonWithJoin < ApplicationRecord
                           foreign_key: "parent_id"
  
   belongs_to :parent, class_name: "PersonWithJoin", optional: true
+
+  def print_tree
+    parent = self.parent
+    value = "{Name :'#{self.name}'"
+    memo = [self.name]
+    while parent!=nil do
+      parent = parent_person.parent
+      if memo.include? parent
+          return 'Cycle detected'
+      end
+      parent_person_name = parent_person.name
+      memo.append(parent_person_name)
+      value.concat(",Parent :{Name :'#{parent_person_name}'}")
+    end
+    value.concat("}")
+    return value
+  end
 end
